38d9cf27cf3b0cb675ed841bba0e18829e63f151
Jarrett Revels <jarrettrevels@gmail.com>
2017-05-24 16:16:08 -0400

    better comments

--- src/primitives/primitives.jl
+++ src/primitives/primitives.jl
@@ -19,7 +19,8 @@ end
 @inline untrack_call(f::F, a, b, c, d, e, others...) where {F} = f(untrack(a), untrack(b), untrack(c), untrack(d), untrack(e), untrack.(others)...)
 
 # This doesn't specialize on DataType arguments naively, so we have to force specialization
-# by unrolling access + type assertions via a generated function.
+# by unrolling access + type assertions via a generated function. This is pretty annoying
+# since the naive method is so simple otherwise...
 @generated function (p::Primitive)(input...)
     typed_input = [:(input[$i]::$(input[i])) for i in 1:nfields(input)]
     return quote
@@ -32,7 +33,7 @@ end
 # If `output` is `Trackable`, then return a tracked version of it
 @inline maybe_track_output(p::Primitive, output, input, ::Trackable) = track(output, p.genre, FunctionNode(p.func, input))
 
-# If `output` is `NotTrackable`, then just return it
+# If `output` is `NotTrackable`, then just return it untracked
 @inline maybe_track_output(p::Primitive, output, input, ::NotTrackable) = output
 
 #############
@@ -68,7 +69,7 @@ macro intercept(expr)
                 hidden_name = Symbol("#cassette_hidden_$(old_name)")
                 lhs.args[1] = hidden_name
             else
-                error("failed to apply Cassette.Intercept to expression $(expr); potentially malformed function signature?")
+                error("failed to apply Cassette.@intercept to expression $(expr); potentially malformed function signature?")
             end
             result = quote
                 $expr
@@ -80,7 +81,7 @@ macro intercept(expr)
             expr.args[2] = :($(Intercept)($(expr.args[2])))
             result = expr
         else
-            error("failed to apply Cassette.Intercept to expression $expr")
+            error("failed to apply Cassette.@intercept to expression $expr")
         end
     else # call site
         result = :($(Intercept)($expr))
