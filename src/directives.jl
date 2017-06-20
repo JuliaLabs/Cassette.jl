00cc8eaa53371ae8326dea7df23a87f6a331708a
Jarrett Revels <jarrettrevels@gmail.com>
2017-06-20 11:12:48 -0700

    flesh out notes and bring back ValueGenre

--- src/directives.jl
+++ src/directives.jl
@@ -1,3 +1,24 @@
+##########
+# Disarm #
+##########
+
+struct Disarm{F} <: Function
+    func::F
+end
+
+@inline Disarm(f::Disarm) = f
+
+@inline func(f::Disarm) = f.func
+
+@inline (f::Disarm{<:Any})(a) = func(f)(value(a))
+@inline (f::Disarm{<:Any})(a, b) = func(f)(value(a), value(b))
+@inline (f::Disarm{<:Any})(a, b, c) = func(f)(value(a), value(b), value(c))
+@inline (f::Disarm{<:Any})(a, b, c, d) = func(f)(value(a), value(b), value(c), value(d))
+@inline (f::Disarm{<:Any})(a, b, c, d, e) = func(f)(value(a), value(b), value(c), value(d), value(e))
+@inline (f::Disarm{<:Any})(a, b, c, d, e, args...) = func(f)(value(a), value(b), value(c), value(d), value(e), value.(args)...)
+
+Base.show(io::IO, f::Disarm) = print(io, typeof(f), "()")
+
 #######################
 # SpecializedFunction #
 #######################
@@ -21,6 +42,8 @@ end
 @inline (f::SpecializedFunction{<:Any})(a, b, c, d) = func(f)(unwrap(a), unwrap(b), unwrap(c), unwrap(d))
 @inline (f::SpecializedFunction{<:Any})(a, b, c, d, e) = func(f)(unwrap(a), unwrap(b), unwrap(c), unwrap(d), unwrap(e))
 @inline (f::SpecializedFunction{<:Any})(a, b, c, d, e, args...) = func(f)(unwrap(a), unwrap(b), unwrap(c), unwrap(d), unwrap(e), unwrap.(args)...)
+
+Base.show(io::IO, f::SpecializedFunction) = print(io, typeof(f), "()")
 
 ##############
 # Directives #
@@ -50,12 +73,16 @@ end
 # ValueGenre #
 ##############
 
-# TODO
+@inline (p::Play{ValueGenre})(input...) = Disarm(func(p))(input...)
+
+@inline (r::Record{ValueGenre})(output::Union{Real,AbstractArray}, input) = ValueNote(output, FunctionNote{ValueGenre}(func(r), input))
+@inline (r::Record{ValueGenre})(output::Bool, input) = output
+@inline (r::Record{ValueGenre})(output, input) = output
 
 #############
 # VoidGenre #
 #############
 
-@inline (p::Play{VoidGenre})(input...) = func(p)(input...)
+@inline (p::Play{VoidGenre})(input...) = Disarm(func(p))(input...)
 
-@inline (r::Record{VoidGenre})(output, input, cache...) = output
+@inline (r::Record{VoidGenre})(output, input) = output
--- src/notes.jl
+++ src/notes.jl
@@ -2,24 +2,156 @@
 # Note #
 ########
 
-mutable struct Note{G<:AbstractGenre,V,P,C}
-    value::V
-    parent::Note{G,P}
+abstract type AbstractNote{G<:AbstractGenre} end
+
+mutable struct FunctionNote{G<:AbstractGenre,V,P<:Tuple,C} <: AbstractNote{G}
+    value::SpecializedFunction{V}
+    parent::P
     cache::C
+    FunctionNote{G}(value::SpecializedFunction{V}, parent::P, cache::C = nothing) where {G,V,P,C} = new{G,V,P,C}(value, parent, cache)
+    FunctionNote{G}(value::V, parent::P, cache::C = nothing) where {G,V,P,C} = FunctionNote{G}(SpecializedFunction(value), parent, cache)
+end
+
+mutable struct ValueNote{G<:AbstractGenre,V,P,C} <: AbstractNote{G}
+    value::V
+    parent::FunctionNote{G,P}
+    cache::C
+    ValueNote{G}(value::V, parent::FunctionNote{G,P}, cache::C = nothing)  where {G,V,P,C} = new{G,V,P,C}(value, parent, cache)
+    ValueNote{G}(value::V, cache::C = nothing) where {G,V,C} = ValueNote{G}(value, FunctionNote{G}(nothing, tuple(), nothing), cache)
+end
+
+ValueNote(value, parent::FunctionNote{G}, cache...) where {G} = ValueNote{G}(value, parent, cache...)
+
+##############
+# Properties #
+##############
+
+# getters #
+#---------#
+
+@inline value(x) = x
+@inline value(note::ValueNote) = note.value
+@inline value(note::FunctionNote) = note.value
+@inline value(::Type{T}) where {T} = valuetype(T)
+
+@inline parent(note::ValueNote) = note.parent
+@inline parent(note::FunctionNote) = note.parent
+
+@inline cache(note::ValueNote) = note.cache
+@inline cache(note::FunctionNote) = note.cache
+
+@inline genre(::Type{AbstractNote{G}}) where {G} = G()
+@inline genre(::Type{ValueNote{G,V,P,C}}) where {G,V,P,C} = G()
+@inline genre(::Type{FunctionNote{G,V,P,C}}) where {G,V,P,C} = G()
+
+@inline valuetype(x) = valuetype(typeof(x))
+@inline valuetype(::Type{V}) where {V} = V
+@inline valuetype(::Type{ValueNote{G,V,P,C}}) where {G,V,P,C} = V
+@inline valuetype(::Type{FunctionNote{G,V,P,C}}) where {G,V,P,C} = V
+
+@inline cachetype(x) = cachetype(typeof(x))
+@inline cachetype(::Type{V}) where {V} = Void
+@inline cachetype(::Type{ValueNote{G,V,P,C}}) where {G,V,P,C} = C
+@inline cachetype(::Type{FunctionNote{G,V,P,C}}) where {G,V,P,C} = C
+
+# setters #
+#---------#
+
+@inline value!(note::ValueNote, v) = (note.value = v)
+
+@inline cache!(note::ValueNote, c) = (note.cache = c)
+@inline cache!(note::FunctionNote, c) = (note.cache = c)
+
+##########
+# isroot #
+##########
+
+@inline isroot(x) = isroot(typeof(x))
+@inline isroot(::DataType) = true
+@inline isroot(::Type{<:AbstractNote}) = false
+@inline isroot(::Type{<:FunctionNote{<:AbstractGenre,Void}}) = true
+@inline isroot(::Type{<:ValueNote{<:AbstractGenre,<:Any,Void}}) = true
+
+###########
+# rewind! #
+###########
+# TODO: How to handle elementwise ValueNote ancestors?
+
+@inline function rewind!(f!, n::ValueNote, recursive::Bool = false)
+    if recursive
+        recur_rewind!(f!, n)
+    else
+        iter_rewind!(f!, n)
+    end
+    return nothing
+end
+
+# iter_rewind! #
+#--------------#
+
+iter_rewind!(f!, n) = nothing
+
+function iter_rewind!(f!, n::ValueNote)
+    queue = Vector{Any}()
+    push!(queue, n)
+    while !(isempty(queue))
+        current = pop!(queue)
+        f!(current)
+        isroot(current) || iter_rewind_ancestors!(queue, parent(current))
+    end
+    return nothing
+end
+
+@generated function iter_rewind_ancestors!(queue, n::FunctionNote{G,V,P}) where {G<:AbstractGenre,V,P<:Tuple}
+    push_calls = Expr(:block, Any[])
+    for i in 1:nfields(P)
+        T = P.parameters[i]
+        if T <: ValueNote
+            push!(push_calls.args, :(push!(queue, ancestors[$i]::$T)))
+        end
+    end
+    return quote
+        $(Expr(:meta, :noinline))
+        ancestors = parent(n)
+        $(push_calls)
+        return nothing
+    end
+end
+
+# recur_rewind! #
+#---------------#
+
+@inline recur_rewind!(f!, n) = nothing
+
+@inline function recur_rewind!(f!, n::ValueNote)
+    f!(n)
+    isroot(n) || recur_rewind_ancestors!(f!, parent(n))
+    return nothing
+end
+
+@generated function recur_rewind_ancestors!(f!, n::FunctionNote{G,V,P}) where {G<:AbstractGenre,V,P<:Tuple}
+    rewind_calls = Expr(:block, Any[])
+    for i in 1:nfields(P)
+        push!(rewind_calls.args, :(recur_rewind!(f!, ancestors[$i]::$(P.parameters[i]))))
+    end
+    return quote
+        $(Expr(:meta, :noinline))
+        ancestors = parent(n)
+        $(rewind_calls)
+        return nothing
+    end
 end
 
 ###################
-# Getters/Setters #
+# Pretty Printing #
 ###################
 
-value(note::Note) = note.value
+idstring(x) = base(62, object_id(x))
 
-parent(note::Note) = note.parent
+function Base.show(io::IO, n::FunctionNote{G,V}) where {G,V}
+    return print(io, "FunctionNote{$G,$V}($(parent(n)), $(cache(n))")
+end
 
-cache(note::Note) = note.cache
-
-genre(::Type{Note{G,V,P,C}}) = G()
-
-value!(note::Note, v) = (note.value = v)
-
-cache!(note::Note, c) = (note.cache = c)
+function Base.show(io::IO, n::ValueNote{G}) where {G}
+    return print(io, "ValueNote{$G}<$(idstring(n))>($(value(n)), $(cache(n)))")
+end
