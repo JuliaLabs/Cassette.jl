51778a846dfc4f4d6cbacd52bc1b39faaa76478d
Jarrett Revels <jarrettrevels@gmail.com>
2017-06-20 22:51:28 -0700

    add smoke test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,31 @@
 using Cassette
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+########################
+# temporary smoke test #
+########################
+
+function rosenbrock(x)
+    a = one(eltype(x))
+    b = 100 * a
+    result = zero(eltype(x))
+    for i in 1:length(x)-1
+        result += (a - x[i])^2 + b*(x[i+1] - x[i]^2)^2
+    end
+    return result
+end
+
+f = Cassette.Trace{Cassette.ValueGenre}(rosenbrock)
+
+x = rand(1000)
+
+xv = Cassette.ValueNote{Cassette.ValueGenre}.(x)
+yv = f(xv)
+@test isa(yv, Cassette.ValueNote)
+@test Cassette.value(yv) == rosenbrock(x)
+t = Cassette.Tape(yv)
+
+x = rand(1000)
+foreach(Cassette.value!, xv, x)
+Cassette.replay!(t)
+@test Cassette.value(yv) == rosenbrock(x)
