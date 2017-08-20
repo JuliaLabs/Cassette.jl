b2a79844add56e290a916ac17bb2dd80d531213c
Jarrett Revels <jarrettrevels@gmail.com>
2017-08-20 18:24:11 -0400

    some minor tests

--- test/ContextTests.jl
+++ test/ContextTests.jl
@@ -3,13 +3,27 @@ module ContextTests
 using Base.Test
 using Cassette
 
-@test Cassette.iscontextdispatch(:(x:::C))
-@test Cassette.iscontextdispatch(:(x::T:C))
-@test Cassette.iscontextdispatch(:((x::T):C))
-@test !Cassette.iscontextdispatch(:(x::T))
-@test !Cassette.iscontextdispatch(:(x::T::S))
-@test !Cassette.iscontextdispatch(:((x::T)::C))
-@test !Cassette.iscontextdispatch(:x)
-@test !Cassette.iscontextdispatch(:(a + b))
+@test Cassette.is_ctx_dispatch(:(x:::C))
+@test Cassette.is_ctx_dispatch(:(x::T:C))
+@test Cassette.is_ctx_dispatch(:(:::C))
+@test Cassette.is_ctx_dispatch(:(::T:C))
+@test !Cassette.is_ctx_dispatch(:(x::T))
+@test !Cassette.is_ctx_dispatch(:(x::T::S))
+@test !Cassette.is_ctx_dispatch(:(::T))
+@test !Cassette.is_ctx_dispatch(:(::T::S))
+@test !Cassette.is_ctx_dispatch(:x)
+@test !Cassette.is_ctx_dispatch(:(a + b))
+
+@test Cassette.parse_ctx_dispatch(:(x:::C)) === (:x, :Any, :C)
+@test Cassette.parse_ctx_dispatch(:(x::T:C)) === (:x, :T, :C)
+@test Cassette.parse_ctx_dispatch(:(:::C)) === (nothing, :Any, :C)
+@test Cassette.parse_ctx_dispatch(:(::T:C)) === (nothing, :T, :C)
+
+@test Cassette.is_non_ctx_dispatch(:(x::T))
+@test Cassette.is_non_ctx_dispatch(:(x::T::S))
+@test Cassette.is_non_ctx_dispatch(:(::T))
+@test Cassette.is_non_ctx_dispatch(:(::T::S))
+@test !Cassette.is_non_ctx_dispatch(:x)
+@test !Cassette.is_non_ctx_dispatch(:(a + b))
 
 end # module ContextTests
