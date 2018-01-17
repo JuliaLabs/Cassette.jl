67e3e2882675e3bbc04e714c322bc0df5a0a7504
Jarrett Revels <jarrettrevels@gmail.com>
2018-01-17 16:42:15 -0500

    better varargs-based approach to interception

--- src/overdub/reflection.jl
+++ src/overdub/reflection.jl
@@ -1,6 +1,10 @@
+const OVERDUB_ARGS_SYMBOL = gensym("cassette_overdub_arguments")
+
+const BEGIN_OVERDUB_REGION = gensym("cassette_begin_overdub_region")
+
 # Return the CodeInfo method body for signature `S` and `world`,
 # if it exists in the method table. Otherwise, return `nothing`.
-function lookup_method_body(::Type{S}, arg_names::Vector,
+function lookup_method_body(::Type{S},
                             world::UInt = typemax(UInt),
                             debug::Bool = false) where {S<:Tuple}
     if debug
@@ -10,16 +14,7 @@ function lookup_method_body(::Type{S}, arg_names::Vector,
         Core.println("\tWORLD: ", world)
     end
     S.parameters[1].name.module === Core.Compiler && return nothing
-    results = _lookup_method_body(S, arg_names, world)
-    results === nothing && return nothing
-    method, code_info = results
-    debug && Core.println("LOOKED UP METHOD: ", method)
-    debug && Core.println("LOOKED UP CODEINFO: ", code_info)
-    return code_info
-end
 
-function _lookup_method_body(::Type{S}, arg_names::Vector,
-                             world::UInt = typemax(UInt)) where {S<:Tuple}
     # retrieve initial Method + CodeInfo
     _methods = Base._methods_by_ftype(S, -1, world)
     length(_methods) == 1 || return nothing
@@ -29,38 +24,45 @@ function _lookup_method_body(::Type{S}, arg_names::Vector,
     static_params = Any[raw_static_params...]
     code_info = Core.Compiler.retrieve_code_info(method_instance)
     isa(code_info, CodeInfo) || return nothing
+    code_info = Core.Compiler.copy_code_info(code_info)
+    debug && Core.println("FOUND METHOD: ", sprint(show, method))
+    debug && Core.println("FOUND CODEINFO: ", sprint(show, code_info))
 
-    # substitute static parameters/varargs
+    # substitute static parameters
     body = Expr(:block)
     body.args = code_info.code
-    if method.isva
-        nargs = Int64(method.nargs)
-        new_nargs = length(arg_names) + 1
-        if new_nargs < nargs # then the final varargs argument is an empty tuple
-            offset = 0
-            new_slots = Any[SlotNumber(i) for i in 1:(nargs - 1)]
-            push!(new_slots, Expr(:call, GlobalRef(Core, :tuple)))
-            Base.Core.Compiler.substitute!(body, nargs, new_slots, method_signature, static_params, offset, :propagate)
-        else
-            new_slotnames = code_info.slotnames[1:(nargs - 1)]
-            new_slotflags = code_info.slotflags[1:(nargs - 1)]
-            for i in nargs:new_nargs
-                push!(new_slotnames, arg_names[i - 1])
-                push!(new_slotflags, 0x00)
-            end
-            append!(new_slotnames, code_info.slotnames[(nargs + 1):end])
-            append!(new_slotflags, code_info.slotflags[(nargs + 1):end])
-            offset = new_nargs - nargs
-            vararg_tuple = Expr(:call, GlobalRef(Core, :tuple), [SlotNumber(i) for i in nargs:new_nargs]...)
-            new_slots = Any[SlotNumber(i) for i in 1:(nargs - 1)]
-            push!(new_slots, vararg_tuple)
-            Base.Core.Compiler.substitute!(body, new_nargs, new_slots, method_signature, static_params, offset, :propagate)
-            code_info.slotnames = new_slotnames
-            code_info.slotflags = new_slotflags
-        end
-    else
-        Base.Core.Compiler.substitute!(body, 0, Any[], method_signature, static_params, 0, :propagate)
-    end
+    Core.Compiler.substitute!(body, 0, Any[], method_signature, static_params, 0, :propagate)
 
-    return method, code_info
+    # construct new slotnames/slotflags, offset non-self slotnumbers
+    code_info.slotnames = Any[code_info.slotnames[1], OVERDUB_ARGS_SYMBOL, code_info.slotnames[2:end]...]
+    code_info.slotflags = Any[code_info.slotflags[1], 0x00,                code_info.slotflags[2:end]...]
+    replace_match!(s -> SlotNumber(s.id + 1), s -> isa(s, SlotNumber) && s.id > 1, code_info.code)
+
+    # construct new `code_info.code` in which original arguments are properly destructured
+    new_code = copy_prelude_code(code_info.code)
+    prelude_end = length(new_code)
+    n_actual_args = fieldcount(S) - 1
+    n_method_args = Int64(method.nargs) - 1
+    for i in 1:n_method_args
+        slot = i + 2
+        actual_argument = Expr(:call, GlobalRef(Core, :getfield), SlotNumber(2), i)
+        push!(new_code, :($(SlotNumber(slot)) = $actual_argument))
+        code_info.slotflags[slot] |= 0x01 << 0x01 # make sure the "assigned" bitflag is set
+    end
+    if method.isva
+        isempty(new_code) || pop!(new_code) # remove the slot reassignment that we're replacing
+        final_arguments = Expr(:call, GlobalRef(Core, :tuple))
+        for i in n_method_args:n_actual_args
+            ssaval = SSAValue(code_info.ssavaluetypes)
+            actual_argument = Expr(:call, GlobalRef(Core, :getfield), SlotNumber(2), i)
+            push!(new_code, :($ssaval = $actual_argument))
+            push!(final_arguments.args, ssaval)
+            code_info.ssavaluetypes += 1
+        end
+        push!(new_code, :($(SlotNumber(n_method_args + 2)) = $final_arguments))
+    end
+    push!(new_code, BEGIN_OVERDUB_REGION)
+    append!(new_code, code_info.code[(prelude_end + 1):end])
+    code_info.code = fix_labels_and_gotos!(new_code)
+    return code_info
 end
