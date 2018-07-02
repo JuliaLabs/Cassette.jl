9b8b965e93499080811cb6bd4f9fbb3e117ba219
Jarrett Revels <jarrettrevels@gmail.com>
2018-07-02 15:49:58 +0200

    extend broadcast tagging test further

--- test/ExampleTests.jl
+++ test/ExampleTests.jl
@@ -287,11 +287,23 @@ result = Cassette.overdub(ctx, broadcast, (v, m) -> Cassette.tag(v, ctx, m), v, 
 @test Cassette.untagtype(typeof(result), typeof(ctx)) === typeof(v)
 @test Cassette.istagged(result, ctx)
 @test Cassette.istaggedtype(typeof(result), typeof(ctx))
-
 @test Cassette.metadata(result, ctx) === Cassette.NoMetaData()
 @test m == map(Cassette.metameta(result, ctx)) do x
     @test x.meta === Cassette.NoMetaMeta()
     return x.data
+end
+@test !Cassette.hasmetadata(result, ctx)
+@test Cassette.hasmetameta(result, ctx)
+
+result = Cassette.@overdub(ctx, ((v, m) -> Cassette.tag(v, ctx, m)).(v, m[1]))
+@test Cassette.untag(result, ctx) == v
+@test Cassette.untagtype(typeof(result), typeof(ctx)) === typeof(v)
+@test Cassette.istagged(result, ctx)
+@test Cassette.istaggedtype(typeof(result), typeof(ctx))
+@test Cassette.metadata(result, ctx) === Cassette.NoMetaData()
+foreach(Cassette.metameta(result, ctx)) do x
+    @test x.meta === Cassette.NoMetaMeta()
+    @test x.data === m[1]
 end
 @test !Cassette.hasmetadata(result, ctx)
 @test Cassette.hasmetameta(result, ctx)
