91ff2803388992de0cd3c4e2ed57600b37a2b94e
Jarrett Revels <jarrettrevels@gmail.com>
2018-08-05 09:23:10 -0400

    fix boundscheck argument untagging in tagged_getfield/setfield

--- src/tagged.jl
+++ src/tagged.jl
@@ -556,33 +556,65 @@ end
 
 #=== tagged_getfield ===#
 
-tagged_getfield(context::ContextWithTag{T}, x, name, boundscheck...) where {T} = getfield(x, untag(name, context), boundscheck...)
+tagged_getfield(context::ContextWithTag{T}, x, name, boundscheck) where {T} = getfield(x, untag(name, context), untag(boundscheck, context))
 
-function tagged_getfield(context::ContextWithTag{T}, x::Tagged{T}, name, boundscheck...) where {T}
+tagged_getfield(context::ContextWithTag{T}, x, name) where {T} = getfield(x, untag(name, context))
+
+function tagged_getfield(context::ContextWithTag{T}, x::Tagged{T}, name, boundscheck) where {T}
+    untagged_boundscheck = untag(boundscheck, context)
     untagged_name = untag(name, context)
-    y_value = getfield(untag(x, context), untagged_name, boundscheck...)
+    y_value = getfield(untag(x, context), untagged_name, untagged_boundscheck)
     x_value = untag(x, context)
     if isa(x_value, Module)
         return tagged_globalref(context, x, untagged_name, getfield(x_value, untagged_name))
     elseif hasmetameta(x, context)
-        y_meta = load(getfield(x.meta.meta, untagged_name, boundscheck...))
+        y_meta = load(getfield(x.meta.meta, untagged_name, untagged_boundscheck))
     else
         y_meta = NOMETA
     end
     return Tagged(context, y_value, y_meta)
 end
 
+function tagged_getfield(context::ContextWithTag{T}, x::Tagged{T}, name) where {T}
+    untagged_name = untag(name, context)
+    y_value = getfield(untag(x, context), untagged_name)
+    x_value = untag(x, context)
+    if isa(x_value, Module)
+        return tagged_globalref(context, x, untagged_name, getfield(x_value, untagged_name))
+    elseif hasmetameta(x, context)
+        y_meta = load(getfield(x.meta.meta, untagged_name))
+    else
+        y_meta = NOMETA
+    end
+    return Tagged(context, y_value, y_meta)
+end
+
+
 #=== tagged_setfield! ===#
 
-tagged_setfield!(context::ContextWithTag{T}, x, name, y, boundscheck...) where {T} = setfield!(x, untag(name, context), y, boundscheck...)
+tagged_setfield!(context::ContextWithTag{T}, x, name, y, boundscheck) where {T} = setfield!(x, untag(name, context), y, untag(boundscheck, context))
 
-function tagged_setfield!(context::ContextWithTag{T}, x::Tagged{T}, name, y, boundscheck...) where {T}
+tagged_setfield!(context::ContextWithTag{T}, x, name, y) where {T} = setfield!(x, untag(name, context), y)
+
+function tagged_setfield!(context::ContextWithTag{T}, x::Tagged{T}, name, y, boundscheck) where {T}
+    untagged_boundscheck = untag(boundscheck, context)
     untagged_name = untag(name, context)
     y_value = untag(y, context)
     y_meta = istagged(y, context) ? y.meta : NOMETA
-    setfield!(x.value, untagged_name, y_value, boundscheck...)
+    setfield!(x.value, untagged_name, y_value, untagged_boundscheck)
     if hasmetameta(x, context)
-        store!(getfield(x.meta.meta, untagged_name, boundscheck...), y_meta)
+        store!(getfield(x.meta.meta, untagged_name, untagged_boundscheck), y_meta)
+    end
+    return y
+end
+
+function tagged_setfield!(context::ContextWithTag{T}, x::Tagged{T}, name, y) where {T}
+    untagged_name = untag(name, context)
+    y_value = untag(y, context)
+    y_meta = istagged(y, context) ? y.meta : NOMETA
+    setfield!(x.value, untagged_name, y_value)
+    if hasmetameta(x, context)
+        store!(getfield(x.meta.meta, untagged_name), y_meta)
     end
     return y
 end
