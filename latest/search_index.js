e2bdf40b11772dbf116b51019e23dd58bf761196
autodocs <autodocs>
2018-08-08 08:38:42 +0000

    build based on 4fa855f

--- latest/search_index.js
+++ latest/search_index.js
@@ -89,17 +89,25 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "contextualtagging.html#",
-    "page": "Contextual Tagging of Values",
-    "title": "Contextual Tagging of Values",
-    "category": "page",
-    "text": "Documentation Coming Soon!"
-},
-
-{
     "location": "contextualpass.html#",
     "page": "Contextual Compiler Pass Injection",
     "title": "Contextual Compiler Pass Injection",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "contextualpass.html#Contextual-Compiler-Pass-Injection-1",
+    "page": "Contextual Compiler Pass Injection",
+    "title": "Contextual Compiler Pass Injection",
+    "category": "section",
+    "text": "CurrentModule = CassetteIn the previous section on Cassette\'s overdubbing mechanism, we explored how Cassette can automatically transform methods\' lowered representation to insert a bunch of statements around method calls encountered while overdubbing. In the section that followed, we discussed the result of this IR transformation: Cassette\'s contextual dispatch interface, a suite of normal Julia methods that can be easily overloaded to perform the kinds of method replacement and instrumentation that would otherwise require manually implemented compiler passes.Some use cases, however, require the ability to access and/or alter properties of the execution trace that just can\'t be reached via simple method overloading, like control flow or the surrounding scope of a method call.To facilitate these use cases, Cassette allows users to write and inject their own arbitrary post-lowering, pre-inference compiler passes as part of the overdubbing process. This feature of Cassette is called \"contextual pass injection\"."
+},
+
+{
+    "location": "contextualtagging.html#",
+    "page": "Contextual Tagging of Values",
+    "title": "Contextual Tagging of Values",
     "category": "page",
     "text": "Documentation Coming Soon!"
 },
